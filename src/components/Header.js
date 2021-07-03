755e4727474664e89c461741392002b4c95d9f1c
Nay Myo Lwin <naymyolwin@gmail.com>
2021-07-03 12:08:19 +0700

    tab added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import { makeStyles } from "@material-ui/core/styles";
 
 import logo from "../assets/logo.svg";
@@ -13,6 +16,14 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     height: "7em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
   },
 }));
 
@@ -35,6 +46,13 @@ const Header = (props) => {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="Company Logo" />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
